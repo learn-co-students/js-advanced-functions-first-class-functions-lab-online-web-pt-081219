ec5b14eb4d8218046f1e434da3f2ee4729408752
kierra <51520037+kierrakay@users.noreply.github.com>
2020-03-31 16:33:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 // Code your solution in this file!
-const returnFirstTwoDrivers = function (array) {
-return array.slice(1,2)
-}
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0, 2)
+    };
+// const returnFirstTwoDrivers = function (array) {
+// return array.slice(0, 2)
+// }
+
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2)
+    };
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+// allows us to invoke either function from the array:
+
+const createFareMultiplier = function (value) {
+    return function(secondValue) {
+        return value * secondValue;
+    };
+};
+
+const fareDoubler = createFareMultiplier(2)
+ const fareTripler = createFareMultiplier(3)
+
+ const selectDifferentDrivers = function (drivers, whichDrivers) {
+     return whichDrivers (drivers);
+ }
